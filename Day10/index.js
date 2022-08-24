880f7a154c2dd980d3cc41adcdc3dc9d81c4c777
Sasikumar <v-ssasikumar@microsoft.com>
2022-08-24 14:15:06 +0530

    Day10 Resource

---
+++ Day10/index.js
@@ -0,0 +1,35 @@
+const fruits = ["apple", "orange", "cherry"];
+
+// Sample 1
+// const newfruits= fruits.map(
+//     (items,index) => {
+//         console.log("S.No:",index,"- item -",items)
+//         // if(items === 'apple')
+//         // return "S.No: "+index+"- item - "+items
+//         return items
+//     }
+//     )
+
+// Sample 2
+// fruits.forEach(
+//     (items) => {
+//         console.log("item -",items) 
+//     }
+// )
+
+//Sample 3
+// const newfruits= fruits.filter(
+//     (items) => items !=='apple'
+//     )cl
+
+    //Sample 4
+// const newfruits= fruits.find(
+//     (items) => items !=='apple'
+//     )
+
+// fruits.push("mango")
+// console.log("newfruits",fruits)
+// fruits.pop()
+// console.log("newfruits",fruits)
+// fruits.splice(0,10)
+// console.log("newfruits",fruits)
