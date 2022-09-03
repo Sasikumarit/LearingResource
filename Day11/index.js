62fd4511067634f189458dbd79fcc773f73e0f06
Sasikumar <v-ssasikumar@microsoft.com>
2022-09-03 19:22:04 +0530

    Day11 Resource

---
+++ Day11/index.js
@@ -0,0 +1,10 @@
+
+ //External Javascript
+ const onsavehandler=()=>{
+    let ItemNamevalue=document.getElementById('ItemName').value
+    let ItemCodevalue=document.getElementById('ItemCode').value;
+    let returndata="Enetered ItemCode is " +ItemCodevalue+ ' and ItemName is '+ ItemNamevalue
+     alert(returndata)
+ }
+
+ 
